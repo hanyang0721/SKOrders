98fbc2ad089cd71835f2366f61f21f08f2ca6c73
hanyang0721 <hanyang0721@gmail.com>
2019-12-28 18:05:13 +0800

    Revert "message"
    This reverts commit 043eea9b63123ab9478c7aecb5a2b6e19bd99602.

--- linepush.py
+++ linepush.py
@@ -23,3 +23,16 @@ except LineBotApiError as e:
     raise e
 
 
+"""
+#圖片訊息
+# ImageSendMessage物件中的輸入
+# original_content_url 以及 preview_image_url都要寫才不會報錯。
+#輸入的網址要是一個圖片，應該說只能是一個圖片，不然不會報錯但是傳過去是灰色不能用的圖
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+image_url = "https://i.imgur.com/eTldj2E.png?1"
+try:
+    line_bot_api.push_message(to, ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
+except LineBotApiError as e:
+    # error handle
+    raise e
+"""
